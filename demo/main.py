c9c3d243be35595366b940847e9b8e14792766f8
samuel-2004 <samuel@lewis.id.au>
2025-08-16 15:12:29 +0930

    add tabs

--- demo/main.py
+++ demo/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def index():
-    return render_template("/edit_pts/edit_pt.html")
+    return render_template("edit_pts/edit_pt.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
