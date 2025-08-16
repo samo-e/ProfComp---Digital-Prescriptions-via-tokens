7d5942f5b044340fcc8c8a52dce8906afad7f1c0
samuel-2004 <samuel@lewis.id.au>
2025-08-16 21:01:20 +0930

    Edit css href links

--- demo/main.py
+++ demo/main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__, template_folder=".")
 
 @app.route("/")
 def index():
-    return render_template("teacher_dashboard/teacher_dash.html")
+    return render_template("edit_pts/edit_pt.html")
+
+@app.route("/edit_pts/edit_pt.css")
+def edit_pt_css():
+    return send_from_directory(os.path.join(app.root_path, "edit_pts"), "edit_pt.css")
 
 if __name__ == "__main__":
     app.run(debug=True)
