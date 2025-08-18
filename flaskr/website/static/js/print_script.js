4c42ad7e1ccc4f8e78b91487f1a61b559d4f7b96
samuel-2004 <samuel@lewis.id.au>
2025-08-18 18:14:58 +0930

    print to pdf

--- flaskr/website/static/js/print_script.js
+++ flaskr/website/static/js/print_script.js
@@ -16,5 +16,49 @@ function print_scripts() {
     }
   });
 
-  console.log(asls_to_print);
+  printIdToPDF(asls_to_print);
+}
+
+function printIdToPDF(rowIds) {
+  const { jsPDF } = window.jspdf;
+  const pdf = new jsPDF();
+  let title = 'Active Script Lists for <pt_id> on <todays date>';
+
+  // Set metadata
+  pdf.setProperties({
+    title: title, // TODO
+    subject: 'List of selected rows',
+    //author: '',
+    //creator: 'My Company' // maybe make this as the student's id?
+  });
+
+  // Format PDF
+  // Add title
+  pdf.setFontSize(16);
+  pdf.text(title, 10, 10);
+
+  // Add each asl row
+  pdf.setFontSize(12);
+  rowIds.forEach((id, index) => {
+    pdf.text(id, 10, 20 + index * 10);
+  });
+
+  // Open a new tab
+  const newTab = window.open('', '_blank');
+  if (!newTab) return;
+
+  // Add content to new tab
+  const iframe = newTab.document.createElement('iframe');
+  iframe.src = pdf.output('datauristring');
+  Object.assign(iframe.style, {
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    border: 'none'
+  });
+
+  newTab.document.body.innerHTML = '';
+  newTab.document.body.appendChild(iframe);
 }
