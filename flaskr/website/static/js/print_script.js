732bd330311159c27a29565025cf51577a307b74
samuel-2004 <samuel@lewis.id.au>
2025-08-22 12:17:50 +0930

    update to using html2pdf

--- flaskr/website/static/js/print_script.js
+++ flaskr/website/static/js/print_script.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   $("#script-print").click(print_scripts);
 });
 
+const options = {
+  filename: 'my-document.pdf',
+  margin: 1,
+  image: { type: 'jpeg', quality: 0.98 },
+  html2canvas: { scale: 2 },
+  jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+};
 
 function print_scripts() {
   let asls_to_print = [];
@@ -11,73 +18,18 @@ function print_scripts() {
   $("#asl-table .asl-paperless").each(function() {
     if ((select_all) || ($(this).find(".asl-check input").is(":checked"))) {
       let this_id = $(this).attr("id")
-      //console.log();
       asls_to_print.push(this_id);
     }
   });
 
-  create_prescription();
-  //printIdToPDF(asls_to_print);
+  appendPrescription();
 }
 
-function printIdToPDF(rowIds) {
-  const { jsPDF } = window.jspdf;
-  const pdf = new jsPDF();
-  let title = 'Active Script Lists for <pt_id> on <todays date>'; // TODO
-
-  // Set metadata
-  pdf.setProperties({
-    title: title,
-    subject: 'List of selected rows',
-    // author: '',
-    // creator: 'My Company'
-  });
-
-  // Create a container for all rows
-  const tempDiv = document.createElement("div");
-
-  rowIds.forEach(id => {
-    const element = $(`#${id}`)[0];
-    if (element) {
-      const wrapper = document.createElement("div");
-      wrapper.style.marginBottom = "4px";       // spacing
-      wrapper.style.maxWidth = "180mm";         // limit width
-      wrapper.style.fontSize = "10px";          // smaller font
-      wrapper.style.lineHeight = "1.2";         // tighter lines
-      wrapper.style.overflowWrap = "break-word";
-      wrapper.style.display = "inline-block";
-      wrapper.innerHTML = element.innerHTML;
-
-      tempDiv.appendChild(wrapper);
-    }
-  });
-  tempDiv.style.color = "red";
-  console.log(tempDiv);
-
-  // Render all content to PDF and open in new tab
-  pdf.html(tempDiv, {
-    x: 10,
-    y: 10,
-    autoPaging: true,
-    callback: function(doc) {
-      const newTab = window.open('', '_blank');
-      if (!newTab) return;
-
-      const iframe = newTab.document.createElement('iframe');
-      iframe.src = doc.output('datauristring');
-      Object.assign(iframe.style, { // Set display options for the opened PDF tab
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        border: 'none'
-      });
-
-      newTab.document.body.innerHTML = '';
-      newTab.document.body.appendChild(iframe);
-    }
-  });
+async function appendPrescription() {
+  let element = await create_prescription();
+  $("body").append(element); // jQuery appends the DOM element
+  const el = document.querySelector(".prescription-container");
+  html2pdf().set(options).from(el).save();
 }
 
 async function create_prescription() {
@@ -92,8 +44,7 @@ async function create_prescription() {
     const container = document.createElement("div");
     container.innerHTML = html;
 
-    // append to body
-    document.body.appendChild(container);
+    return container;
   } catch (error) {
     console.error("Error fetching content:", error);
   }
