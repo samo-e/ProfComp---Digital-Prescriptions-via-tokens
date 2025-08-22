bbbed8a06efa54dd8a343405717a21af591abc69
samuel-2004 <samuel@lewis.id.au>
2025-08-22 15:31:01 +0930

    create filter

--- flaskr/website/static/js/asl.js
+++ flaskr/website/static/js/asl.js
@@ -6,3 +6,32 @@ $(document).ready(function() {
     $icon.toggleClass('bi-chevron-up bi-chevron-down');
   });
 });
+
+$(document).ready(function() {
+  $('#asl-filter').on('keyup', function() {
+    var filter = $(this).val().toUpperCase();
+
+    var tables = [
+      { id: "asl-table", col: 2 },
+      { id: "alr-table", col: 1 }
+    ];
+
+    tables.forEach(function(tableInfo) {
+      var table = document.getElementById(tableInfo.id);
+      if (!table) return;
+
+      var tr = table.getElementsByTagName("tr");
+
+      // Loop through all table rows
+      for (let i = 0; i < tr.length; i++) {
+        var td = tr[i].getElementsByTagName("td")[tableInfo.col];
+        if (td) {
+          var txtValue = td.textContent || td.innerText;
+          tr[i].style.display = txtValue.toUpperCase().indexOf(filter) > -1 ? "" : "none";
+        }
+      }
+    });
+  });
+});
+
+
