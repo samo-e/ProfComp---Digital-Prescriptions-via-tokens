e8d85ba08b80828ccea57506c6cd16fbbd2d6eb7
samuel-2004 <samuel@lewis.id.au>
2025-08-23 15:18:07 +0930

    update to use jquery more consistently

--- flaskr/website/static/js/print_script.js
+++ flaskr/website/static/js/print_script.js
@@ -20,38 +20,8 @@ const options = {
   },
 };
 
-async function print_scripts() {
-  let select_all = ($("#asl-table .asl-paperless .asl-check input:checked").length == 0);
-  
-  // Temp parent container to store the prescriptions
-  let container = document.createElement("div");
-  
-  // Loop through prescriptions and append them into container
-  for (let elem of $("#asl-table .asl-paperless")) {
-    if (select_all || $(elem).find(".asl-check input").is(":checked")) {
-      let this_id = $(elem).attr("id");
-      let presc = await create_prescription();
-      presc = insert_prescription_details(presc, this_id);
-
-      // Add a page break between prescriptions
-      let wrapper = document.createElement("div");
-      wrapper.appendChild(presc);
-      wrapper.style.breakAfter = "page";  
-
-      container.appendChild(wrapper);
-    }
-  }
-
-  if (true) {
-    // Below is debug only (disables auto-download)
-    $("body").append(container); // jQuery appends the DOM element
-    html2pdf().set(options).from(container).outputPdf().get('pdf').then(function (pdfObj) {pdfObj.autoPrint();window.open(pdfObj.output("bloburl"), "F")});;
-  } else {
-    html2pdf().set(options).from(container).save();
-  }
-}
-
-async function create_prescription() {
+//const blank_prescription = await get_blank_prescription();
+async function get_blank_prescription() {
   try {
     // fetch the HTML from the server
     const response = await fetch("/prescription");
@@ -66,6 +36,36 @@ async function create_prescription() {
     return container;
   } catch (error) {
     console.error("Error fetching content:", error);
+  }
+}
+
+async function print_scripts() {
+  let select_all = ($("#asl-table .asl-paperless .asl-check input:checked").length == 0);
+  
+  // Temp parent container to store the prescriptions
+  
+  let $container = $('<div></div>')
+  // Loop through prescriptions and append them into container
+  for (let elem of $("#asl-table .asl-paperless")) {
+    if (select_all || $(elem).find(".asl-check input").is(":checked")) {
+      let this_id = $(elem).attr("id");
+      let presc = await create_prescription();
+      presc = insert_prescription_details(presc, this_id);
+
+      // Add a page break between prescriptions
+      let $wrapper = $('<div></div>').append(presc);
+      $wrapper.css({'break-after': 'page',});
+
+      $container.append(presc);
+    }
+  }
+
+  if (true) {
+    // Below is debug only (disables auto-download)
+    $("body").append($container);
+    html2pdf().set(options).from($container[0]).outputPdf().get('pdf').then(function (pdfObj) {pdfObj.autoPrint();window.open(pdfObj.output("bloburl"), "F")});;
+  } else {
+    html2pdf().set(options).from($container[0]).save();
   }
 }
 
@@ -103,14 +103,21 @@ const drug_class_list = [
 // Add details of the prescription to the element
 // See comment in prescription.html
 // data input accessed via `asl_data`: json
-function insert_prescription_details(el) {
+function insert_prescription_details(el, drug_id) {
   // Append data
   id_list.forEach(id => {
     const $el = $(el).find("#" + id);   // scoped inside el
     if ($el.length && asl_data[id] !== undefined) {
-      $el.text(asl_data[id]);
+      let data_point = asl_data[id];
+      if (data_point === true) {
+        data_point = 'x';
+      } else if (data_point === false){
+        return; //continue
+      }
+      $el.text(data_point);
     }
   });
 
   // Append drug data
+  return el;
 }
