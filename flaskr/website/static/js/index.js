9c05c92663790612ed9ed58a738303db05446c6d
samuel-2004 <samuel@lewis.id.au>
2025-08-23 20:02:20 +0930

    update js formatting

--- flaskr/website/static/js/index.js
+++ flaskr/website/static/js/index.js
@@ -1,16 +1,18 @@
 // Darkmode
 $(document).ready(function () {
-    $(function () {
-      // On page load, check cookie
-      const theme = Cookies.get("theme") || "light";
-      $("html").attr("data-bs-theme", theme);
-      $("#switch-darkmode").prop("checked", theme === "dark");
-
-      // On toggle
-      $("#switch-darkmode").on("change", function () {
-        const newTheme = this.checked ? "dark" : "light";
-        $("html").attr("data-bs-theme", newTheme);
-        Cookies.set("theme", newTheme, { expires: 365 }); // save for 1 year
-      });
-    });
+  load_dark_mode();
 });
+
+function load_dark_mode() {
+  // On page load, check cookie
+  const theme = Cookies.get("theme") || "light";
+  $("html").attr("data-bs-theme", theme);
+  $("#switch-darkmode").prop("checked", theme === "dark");
+
+  // On toggle
+  $("#switch-darkmode").on("change", function () {
+    const newTheme = this.checked ? "dark" : "light";
+    $("html").attr("data-bs-theme", newTheme);
+    Cookies.set("theme", newTheme, { expires: 365 });
+  });
+}
