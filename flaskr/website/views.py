b581c99a6ce02a8f19adf6f0d5069065ac819c41
samuel-2004 <samuel@lewis.id.au>
2025-08-24 17:51:32 +0930

    create delete consent modal

--- flaskr/website/views.py
+++ flaskr/website/views.py
@@ -31,7 +31,7 @@ def asl(pt: int):
         "rpbs" : None,
         "consent-status" : {
             "is-registered" : True,
-            "status" : "No-Consent",
+            "status" : "Rejected", #No-Consent,Pending,Granted,Rejected
             "last-updated" : "10/06/2021 08:27 PM",
         },
         "asl_data" : [
