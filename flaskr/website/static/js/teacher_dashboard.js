d6ecf9e595cda57751883a624f15638a30c7d82d
samo-e <guptasamay@gmail.com>
2025-10-08 10:11:02 +0800

    updated with necessary js code

--- flaskr/website/static/js/teacher_dashboard.js
+++ flaskr/website/static/js/teacher_dashboard.js
@@ -1,22 +1,33 @@
 // flaskr/website/static/js/teacher_dashboard.js
 
+/**
+ * Toggle select all checkboxes for scenarios
+ */
 function selectAllToggle() {
     const selectAll = document.getElementById('select-all');
     const checkboxes = document.getElementsByTagName('input');
     
     for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].type === 'checkbox' && checkboxes[i].id && checkboxes[i].id.indexOf('scenario-check-') === 0) {
+        if (checkboxes[i].type === 'checkbox' && 
+            checkboxes[i].id && 
+            checkboxes[i].id.indexOf('scenario-') === 0) {
             checkboxes[i].checked = selectAll.checked;
         }
     }
 }
 
+/**
+ * Archive selected scenarios in bulk
+ */
 function bulkArchive() {
     const checkboxes = document.getElementsByTagName('input');
     let selected = 0;
     
     for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].type === 'checkbox' && checkboxes[i].id && checkboxes[i].id.indexOf('scenario-check-') === 0 && checkboxes[i].checked) {
+        if (checkboxes[i].type === 'checkbox' && 
+            checkboxes[i].id && 
+            checkboxes[i].id.indexOf('scenario-') === 0 && 
+            checkboxes[i].checked) {
             selected++;
         }
     }
@@ -28,15 +39,23 @@ function bulkArchive() {
     
     if (confirm('Are you sure you want to archive ' + selected + ' selected scenario(s)?')) {
         alert('Bulk archive functionality to be implemented');
+        // TODO: Implement actual bulk archive functionality
+        // This would require a backend endpoint to handle bulk operations
     }
 }
 
+/**
+ * Export selected scenarios in bulk
+ */
 function bulkExport() {
     const checkboxes = document.getElementsByTagName('input');
     let selected = 0;
     
     for (let i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i].type === 'checkbox' && checkboxes[i].id && checkboxes[i].id.indexOf('scenario-check-') === 0 && checkboxes[i].checked) {
+        if (checkboxes[i].type === 'checkbox' && 
+            checkboxes[i].id && 
+            checkboxes[i].id.indexOf('scenario-') === 0 && 
+            checkboxes[i].checked) {
             selected++;
         }
     }
@@ -47,8 +66,13 @@ function bulkExport() {
     }
     
     alert('Bulk export functionality to be implemented');
+    // TODO: Implement actual bulk export functionality
 }
 
+/**
+ * Confirm deletion of a single scenario
+ * @param {number} scenarioId - The ID of the scenario to delete
+ */
 function confirmDelete(scenarioId) {
     if (confirm('Are you sure you want to archive this scenario?')) {
         const form = document.getElementById('delete-form-' + scenarioId);
@@ -58,6 +82,9 @@ function confirmDelete(scenarioId) {
     }
 }
 
+/**
+ * Initialize the page when DOM is loaded
+ */
 window.onload = function() {
     // Set up select all checkbox
     const selectAllBox = document.getElementById('select-all');
@@ -92,9 +119,9 @@ window.onload = function() {
     if (createLink) {
         createLink.onclick = function(e) {
             e.preventDefault();
-            const form = document.querySelector('form[action="{{ url_for("views.create_scenario") }}"]');
-            if (form) {
-                const button = form.querySelector('button[type="submit"]');
+            const forms = document.querySelectorAll('form[action*="create_scenario"]');
+            if (forms.length > 0) {
+                const button = forms[0].querySelector('button[type="submit"]');
                 if (button) {
                     button.click();
                 }
